89bfb4f27b6b921c48b0879503d652936792f010
Yamazaki Norihito <kizinn1224@gmail.com>
2021-08-30 23:16:31 +0900

    sgnin actionc add

---
+++ kao-net-app/Controllers/Auth/StateController.cs
@@ -0,0 +1,46 @@
+﻿using kao_net_app.Model.Auth.Account;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Firebase.Auth;
+using kao_net_app.Model.Auth.State;
+
+namespace kao_net_app.Controllers.Auth
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class StateController : AbsAuthController
+    {
+
+        [HttpPost]
+        [Route("[action]")]
+        public FirebaseAuth SignIn([FromBody] SignInModel requestdata)
+        {
+            var signInTask = _FirebaseAuthProvider.SignInWithEmailAndPasswordAsync(
+                                                    requestdata.Email,
+                                                    requestdata.Password);
+
+            signInTask.Wait();
+
+            FirebaseAuthLink signInResult = signInTask.Result;
+
+
+            FirebaseAuth response = null;
+
+            if (string.IsNullOrEmpty(signInResult.FirebaseToken))
+            {// error
+            }
+            else
+            {// success
+                response = signInResult;
+            }
+
+            return response;
+        }
+
+    }
+}
---
+++ kao-net-app/Model/Auth/State/SignInModel.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace kao_net_app.Model.Auth.State
+{
+    public class SignInModel
+    {
+        [JsonProperty(PropertyName = "email")]
+        public string Email { get; set; }
+
+        [JsonProperty(PropertyName = "password")]
+        public string Password { get; set; }
+
+    }
+}
