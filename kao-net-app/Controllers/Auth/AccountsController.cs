b11975e69fbce9b12f7f592b1a3b736582e7419c
Yamazaki Norihito <kizinn1224@gmail.com>
2021-08-30 22:19:43 +0900

    dev auth account reg action add

---
+++ kao-net-app/Controllers/Auth/AccountsController.cs
@@ -0,0 +1,27 @@
+﻿using kao_net_app.Model.Auth.Account;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kao_net_app.Controllers.Auth
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AccountsController : AbsBaseController
+    {
+        [HttpPost]
+        public JsonResult Regist([FromBody] RegistModel data)
+        {
+            object response = new
+            {
+                data
+            };
+
+            return new JsonResult(response);
+        }
+
+    }
+}
---
+++ kao-net-app/Model/Auth/Account/RegistModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace kao_net_app.Model.Auth.Account
+{
+    public class RegistModel
+    {
+        [JsonProperty(PropertyName = "email")]
+        public string Email { get; set; }
+
+        [JsonProperty(PropertyName = "password")]
+        public string Password { get; set; }
+
+        [JsonProperty(PropertyName = "passwordConfirm")]
+        public string Password_Confirm { get; set; }
+
+    }
+}
