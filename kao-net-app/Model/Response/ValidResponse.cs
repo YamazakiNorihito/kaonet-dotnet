9c0eefcb0ee0958a787d2b381b68c18f9ae49acc
Yamazaki Norihito <kizinn1224@gmail.com>
2021-09-29 22:04:02 +0900

    Exception response json convert エラー修正

--- kao-net-app/Model/Response/ValidResponse.cs
+++ kao-net-app/Model/Response/ValidResponse.cs
@@ -1,20 +1,18 @@
-﻿
-using System;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Text.Encodings.Web;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Text.Unicode;
 
+using System.Linq;
+using System.Web;
+
 namespace kao_net_app.Model.Response
 {
     public class ValidResponse : AbsBaseResponse
     {
-        private static JsonSerializerOptions options = new JsonSerializerOptions()
-        {
-            // すべての言語セットをエスケープせずにシリアル化させる
-            Encoder = JavaScriptEncoder.Create(UnicodeRanges.All)
-        };
-
         [JsonPropertyName("message")]
         public string Message { get; }
 
@@ -23,10 +21,26 @@ namespace kao_net_app.Model.Response
             this.Message = message;
         }
 
-
-        public ValidResponse(Exception ex) : this(System.Text.Json.JsonSerializer.Serialize(ex, options))
+        public ValidResponse(Exception ex) : base(ResponseStatus.VALID)
         {
+            var error = new Dictionary<string, string>
+            {
+                {"Type", ex.GetType().ToString()},
+                {"Message", ex.Message},
+                {"StackTrace", ex.StackTrace}
+            };
+            foreach (DictionaryEntry data in ex.Data)
+            {
+                error.Add(data.Key.ToString(), data.Value.ToString());
+            }
 
+            var options = new JsonSerializerOptions()
+            {
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                WriteIndented = true
+            };
+
+            this.Message = System.Text.Json.JsonSerializer.Serialize(error, options);
         }
     }
 }
